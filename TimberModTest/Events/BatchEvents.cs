c086ae7fa4e6c772b7b423f08a802148ca65ce8f
Thomas Price <thomaswprice1@gmail.com>
2024-01-11 22:28:49 -0500

    Attempt at good distribution event

--- TimberModTest/Events/BatchEvents.cs
+++ TimberModTest/Events/BatchEvents.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 using System.Text;
 using Timberborn.BlockObjectTools;
 using Timberborn.Buildings;
+using Timberborn.DistributionSystem;
+using Timberborn.DistributionSystemBatchControl;
 using Timberborn.EntitySystem;
 using Timberborn.GameDistricts;
 using Timberborn.GameDistrictsMigration;
 using Timberborn.GameDistrictsMigrationBatchControl;
+using UnityEngine.UIElements;
 using static UnityEngine.GraphicsBuffer;
+using static UnityEngine.InputSystem.InputRemoting;
 
 namespace TimberModTest.Events
 {
@@ -94,4 +98,123 @@ namespace TimberModTest.Events
         }
     }
 
+    //[Serializable]
+    //class ExportThresholdChangedEvent : ReplayEvent
+    //{
+    //    public string districtEntityID;
+    //    public string goodID;
+    //    public float threshold;
+
+    //    public override void Replay(IReplayContext context)
+    //    {
+    //        var distributionSetting = GetComponent<DistrictDistributionSetting>(context, districtEntityID);
+    //        if (distributionSetting != null) return;
+    //        var goodSetting = distributionSetting.GetGoodDistributionSetting(goodID);
+    //        if (goodSetting != null)
+    //        {
+    //            Plugin.LogWarning($"Could not find good {goodID} in district {districtEntityID}");
+    //            return;
+    //        }
+    //        goodSetting.SetExportThreshold(threshold);
+    //    }
+
+    //    public override string ToActionString()
+    //    {
+    //        return base.ToActionString();
+    //    }
+    //}
+
+    // This isn't going to work because the slider does not have a reference to
+    // the district.
+    //[HarmonyPatch(typeof(ExportThresholdSlider), nameof(ExportThresholdSlider.OnSliderChanged))]
+    //class ExportThresholdSliderOnSliderChangedPatcher
+    //{
+    //    static bool Prefix(ExportThresholdSlider __instance, ChangeEvent<float> evt)
+    //    {
+    //        return ReplayEvent.DoPrefix(() =>
+    //        {
+    //            var districtEntityID = ReplayEvent.GetEntityID(__instance._setting._goodSpecification.);
+    //            var goodID = __instance._goodDistributionSetting.GoodID;
+    //            return new ExportThresholdChangedEvent()
+    //            {
+    //                districtEntityID = districtEntityID,
+    //                goodID = goodID,
+    //                threshold = value
+    //            };
+    //        });
+    //    }
+    //}
+
+    [Serializable]
+    class GoodDistributionSettingChangedEvent : ReplayEvent
+    {
+        public string districtEntityID;
+        public string goodID;
+        public float threshold;
+        public ImportOption importOption;
+
+        public override void Replay(IReplayContext context)
+        {
+            var distributionSetting = GetComponent<DistrictDistributionSetting>(context, districtEntityID);
+            if (distributionSetting != null) return;
+            var goodSetting = distributionSetting.GetGoodDistributionSetting(goodID);
+            if (goodSetting != null)
+            {
+                Plugin.LogWarning($"Could not find good {goodID} in district {districtEntityID}");
+                return;
+            }
+            if (goodSetting.ExportThreshold != threshold)
+            {
+                goodSetting.SetExportThreshold(threshold);
+            }
+            if (goodSetting.ImportOption != importOption)
+            {
+                goodSetting.SetImportOption(importOption);
+            }    
+        }
+
+        public override string ToActionString()
+        {
+            return base.ToActionString();
+        }
+    }
+
+    [HarmonyPatch(typeof(DistrictDistributionSetting), nameof(DistrictDistributionSetting.AddGoodDistributionSetting))]
+    class DistrictDistributionSettingAddPatcher
+    {
+        static bool Prefix(DistrictDistributionSetting __instance, GoodDistributionSetting goodDistributionSetting)
+        {
+            var districtEntityID = ReplayEvent.GetEntityID(__instance);
+            var goodID = goodDistributionSetting?.GoodId;
+            if (districtEntityID == null || goodID == null) return true;
+
+            __instance._goodDistributionSettings.Add(goodDistributionSetting);
+            goodDistributionSetting.SettingChanged += (sender, e) =>
+            {
+                // If not loaded, proceed as normal
+                if (!ReplayService.IsLoaded)
+                {
+                    __instance.OnSettingChanged(sender, e);
+                    return;
+                };
+
+                // Otherwise record the event as usual
+                var message = new GoodDistributionSettingChangedEvent()
+                {
+                    districtEntityID = districtEntityID,
+                    goodID = goodID,
+                    threshold = goodDistributionSetting.ExportThreshold,
+                    importOption = goodDistributionSetting.ImportOption,
+                };
+                Plugin.Log(message.ToActionString());
+                ReplayService.RecordEvent(message);
+
+                // Only raise the event and do its effects if we're supposed
+                // to play this event
+                if (!EventIO.ShouldPlayPatchedEvents) return;
+                __instance.OnSettingChanged(sender, e);
+            };
+            return false;
+        }
+    }
 }
