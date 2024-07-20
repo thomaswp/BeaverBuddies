7c90e9f73eb7c766788f21355d79c1d0c6735fe0
Thomas Price <thomaswprice1@gmail.com>
2024-07-20 11:43:23 -0400

    Attempted to add vector-friendly serialization logic Not sure why this was only now needed, but maybe a Unity 6 update?

--- BeaverBuddies/FileIO.cs
+++ BeaverBuddies/FileIO.cs
@@ -10,9 +10,57 @@ using Timberborn.SceneLoading;
 using Timberborn.SingletonSystem;
 using BeaverBuddies.Events;
 using TimberNet;
+using UnityEngine;
 
 namespace BeaverBuddies
 {
+    class Vector3IntConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Vector3Int);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var array = JArray.Load(reader);
+            return new Vector3Int(array[0].Value<int>(), array[1].Value<int>(), array[2].Value<int>());
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var vector = (Vector3Int)value;
+            writer.WriteStartArray();
+            writer.WriteValue(vector.x);
+            writer.WriteValue(vector.y);
+            writer.WriteValue(vector.z);
+            writer.WriteEndArray();
+        }
+    }
+
+    class Vector3Converter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Vector3);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var array = JArray.Load(reader);
+            return new Vector3(array[0].Value<float>(), array[1].Value<float>(), array[2].Value<float>());
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var vector = (Vector3)value;
+            writer.WriteStartArray();
+            writer.WriteValue(vector.x);
+            writer.WriteValue(vector.y);
+            writer.WriteValue(vector.z);
+            writer.WriteEndArray();
+        }
+    }
 
     class JsonSettings : JsonSerializerSettings
     {
@@ -21,6 +69,8 @@ namespace BeaverBuddies
             // TODO: Undo for production
             Formatting = Formatting.Indented;
             TypeNameHandling = TypeNameHandling.All;
+            Converters.Add(new Vector3Converter());
+            Converters.Add(new Vector3IntConverter());
         }
 
         public static readonly JsonSettings Default = new JsonSettings();
