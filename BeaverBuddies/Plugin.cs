d27aee9aacadad8a6dc13e5aa1296b9fa674ece2
Thomas Price <thomaswprice1@gmail.com>
2024-09-14 13:47:03 -0400

    v1.2.4

--- BeaverBuddies/Plugin.cs
+++ BeaverBuddies/Plugin.cs
@@ -85,7 +85,7 @@ namespace BeaverBuddies
     {
 
         // TODO: Need to manually keep this updated now
-        public const string Version = "1.2.3";
+        public const string Version = "1.2.4";
         public const string Name = "BeaverBuddies";
         public const string ID = "beaverbuddies";
 
