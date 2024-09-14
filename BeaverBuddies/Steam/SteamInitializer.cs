33e011adb8fd01d266357d672688328339d34b51
Thomas Price <thomaswprice1@gmail.com>
2024-09-14 11:14:24 -0400

    Got a steam invite working!

--- BeaverBuddies/Steam/SteamInitializer.cs
+++ BeaverBuddies/Steam/SteamInitializer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Timberborn.SingletonSystem;
+using Timberborn.SteamStoreSystem;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,10 +11,13 @@ namespace BeaverBuddies.Steam
 {
     class SteamInitializer : IUpdatableSingleton
     {
-        public SteamInitializer() 
+        private SteamManager _steamManager;
+
+        public SteamInitializer(SteamManager steamManager) 
         {
-            GameObject obj = new GameObject("SteamManagerHost");
-            obj.AddComponent<SteamManager>();
+            _steamManager = steamManager;
+            //GameObject obj = new GameObject("SteamManagerHost");
+            //obj.AddComponent<SteamManager>();
         }
 
         bool done = false;
@@ -21,16 +25,37 @@ namespace BeaverBuddies.Steam
         {
             if (!done)
             {
-                if (SteamManager.Initialized)
+                if (_steamManager.Initialized)
                 {
                     string name = SteamFriends.GetPersonaName();
                     Plugin.Log(name);
                     done = true;
+
+                    var lobby = SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
+                    Callback<LobbyCreated_t>.Create(OnLobbyCreated);
                 }
                 else
                 {
                     Plugin.Log("Waiting on Steamworks to initialize...");
                 }
+            }
+        }
+
+        private void OnLobbyCreated(LobbyCreated_t callback)
+        {
+            // Handle the callback
+            if (callback.m_eResult == EResult.k_EResultOK)
+            {
+                // Lobby created successfully
+                CSteamID lobbyId = new CSteamID(callback.m_ulSteamIDLobby);
+                UnityEngine.Debug.Log("Lobby created with ID: " + lobbyId);
+
+                SteamFriends.ActivateGameOverlayInviteDialog(lobbyId);
+            }
+            else
+            {
+                // Handle error
+                UnityEngine.Debug.LogError("Failed to create lobby: " + callback.m_eResult);
             }
         }
     }
